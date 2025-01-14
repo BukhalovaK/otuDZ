79c639b6a38177958c035d24c620173f036b8b5b
Ekaterina Bukhalova <e.bukhalova@yadro.com>
2025-01-14 12:31:41 +0300

    HW4 is complited

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,11 +9,14 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+}
+
+type subValue struct {
+	key   Key
+	value interface{}
 }
 
 func NewCache(capacity int) Cache {
@@ -22,4 +25,38 @@ func NewCache(capacity int) Cache {
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	var exist bool
+	v := subValue{key: key, value: value}
+	if item, ok := c.items[key]; ok {
+		item.Value = v
+		c.queue.MoveToFront(item)
+		exist = true
+	} else {
+		newItem := c.queue.PushFront(v)
+		c.items[key] = newItem
+		if c.queue.Len() > c.capacity {
+			backItem := c.queue.Back()
+			c.queue.Remove(backItem)
+			delete(c.items, backItem.Value.(subValue).key)
+		}
+	}
+
+	return exist
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	if item, ok := c.items[key]; ok {
+		c.queue.MoveToFront(item)
+		return item.Value.(subValue).value, true
+	}
+
+	return nil, false
+}
+
+func (c *lruCache) Clear() {
+	c.queue = NewList()
+	c.items = make(map[Key]*ListItem, c.capacity)
 }
--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -49,8 +49,77 @@ func TestCache(t *testing.T) {
 		require.Nil(t, val)
 	})
 
-	t.Run("purge logic", func(t *testing.T) {
-		// Write me
+	t.Run("delete an item due to queue size", func(t *testing.T) {
+		c := NewCache(3)
+
+		wasInCache := c.Set("aaa", 100)
+		require.False(t, wasInCache)
+
+		wasInCache = c.Set("bbb", 200)
+		require.False(t, wasInCache)
+
+		wasInCache = c.Set("ccc", 300)
+		require.False(t, wasInCache)
+
+		wasInCache = c.Set("ddd", 400)
+		require.False(t, wasInCache)
+
+		val, ok := c.Get("aaa")
+		require.False(t, ok)
+		require.Nil(t, val)
+	})
+
+	t.Run("delete an old item due to queue size", func(t *testing.T) {
+		c := NewCache(3)
+
+		wasInCache := c.Set("aaa", 100) // [(aaa, 100)]
+		require.False(t, wasInCache)
+
+		wasInCache = c.Set("bbb", 200) // [(bbb 200), (aaa, 100)]
+		require.False(t, wasInCache)
+
+		wasInCache = c.Set("ccc", 300) // [(ccc 300), (bbb 200), (aaa, 100)]
+		require.False(t, wasInCache)
+
+		wasInCache = c.Set("aaa", 400) // [(aaa, 400), (ccc 300), (bbb 200)]
+		require.True(t, wasInCache)
+
+		val, ok := c.Get("bbb") // [(bbb 200), (aaa, 400), (ccc 300)]
+		require.True(t, ok)
+		require.Equal(t, 200, val)
+
+		wasInCache = c.Set("ddd", 600) // [(ddd 600), (bbb 200), (aaa, 400)]
+		require.False(t, wasInCache)
+
+		val, ok = c.Get("ccc")
+		require.False(t, ok)
+		require.Nil(t, val)
+	})
+
+	t.Run("clear cache", func(t *testing.T) {
+		c := NewCache(3)
+		wasInCache := c.Set("aaa", 100) // [(aaa, 100)]
+		require.False(t, wasInCache)
+
+		wasInCache = c.Set("bbb", 200) // [(bbb 200), (aaa, 100)]
+		require.False(t, wasInCache)
+
+		wasInCache = c.Set("ccc", 300) // [(ccc 300), (bbb 200), (aaa, 100)]
+		require.False(t, wasInCache)
+
+		c.Clear()
+
+		val, ok := c.Get("ccc")
+		require.False(t, ok)
+		require.Nil(t, val)
+
+		val, ok = c.Get("aaa")
+		require.False(t, ok)
+		require.Nil(t, val)
+
+		val, ok = c.Get("bbb")
+		require.False(t, ok)
+		require.Nil(t, val)
 	})
 }
 
