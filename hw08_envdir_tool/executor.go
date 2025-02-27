01d22b537a2266e57e09e2d64ebc1a6eee38618b
Ekaterina Bukhalova <e.bukhalova@yadro.com>
2025-02-27 16:50:41 +0300

    HW08 is completed

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,45 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func defineEnvVars(env Environment) {
+	for name, v := range env {
+		if err := os.Unsetenv(name); err != nil {
+			fmt.Printf("couldn't unset environment variable %s: %v\n", name, err)
+		}
+		if !v.NeedRemove {
+			if err := os.Setenv(name, v.Value); err != nil {
+				fmt.Printf("couldn't set environment variable %s: %v\n", name, err)
+			}
+		}
+	}
+}
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	if len(cmd) == 0 {
+		fmt.Println("wrong command")
+		return 1
+	}
+
+	cmdName := cmd[0]
+	cmdArgs := cmd[1:]
+
+	defineEnvVars(env)
+
+	command := exec.Command(cmdName, cmdArgs...)
+	command.Env = os.Environ()
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+	command.Stdin = os.Stdin
+
+	if err := command.Run(); err != nil {
+		return 1
+	}
+
+	return 0
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,33 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	env := Environment{
+		"BAR":   EnvValue{Value: "bar"},
+		"EMPTY": EnvValue{NeedRemove: true},
+		"FOO":   EnvValue{Value: "   foo\nwith new line"},
+		"HELLO": EnvValue{Value: "\"hello\""},
+		"UNSET": EnvValue{NeedRemove: true},
+	}
+
+	t.Run("check define env vars", func(t *testing.T) {
+		defineEnvVars(env)
+
+		require.Equal(t, "bar", os.Getenv("BAR"))
+		require.Equal(t, "", os.Getenv("EMPTY"))
+		require.Equal(t, "   foo\nwith new line", os.Getenv("FOO"))
+		require.Equal(t, "\"hello\"", os.Getenv("HELLO"))
+		require.Equal(t, "", os.Getenv("UNSET"))
+	})
+
+	t.Run("check exec command", func(t *testing.T) {
+		code := RunCmd([]string{"echo"}, env)
+		require.Equal(t, 0, code)
+	})
 }
